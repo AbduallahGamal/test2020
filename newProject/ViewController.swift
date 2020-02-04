70b23828eba80288f485fa242b4460df72e332fe
Abduallah Gamal <abduallahgamal@gmail.com>
2020-02-04 12:41:47 +0200

    Initial Commit

---
+++ newProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  newProject
+//
+//  Created by Abdalla on 2/4/20.
+//  Copyright © 2020 edu.data. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
